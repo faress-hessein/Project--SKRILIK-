b1e4055971e12bbfde5a2abec041f90b4c2fc81c
faress <faresshessen16@gmail.com>
2025-03-01 13:29:25 +0300

    edite-form

--- master.js
+++ master.js
@@ -7,56 +7,56 @@ let form = document.querySelector("form");
 let msgForm = document.querySelector(".msgForm");
 
 // let  send form
-function sendEmail() {
-  let bodyMassege = `Name: ${YourName.value} <br> 
-    Email: ${YourEmail.value} <br>
-    Phone: ${YourPhone.value} <br>
-    From Wep: "SKRILIK"`;
+// function sendEmail() {
+//   let bodyMassege = `Name: ${YourName.value} <br>
+//     Email: ${YourEmail.value} <br>
+//     Phone: ${YourPhone.value} <br>
+//     From Wep: "SKRILIK"`;
 
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "faresshessen61@gmail.com",
-    Password: "DB87645B09CFA82F10DB8793B350DBDD0679",
-    To: "faresshessen61@gmail.com",
-    From: "faresshessen61@gmail.com",
-    Subject: YourName.value,
-    Body: bodyMassege,
-  });
-  YourName.value = "";
-  YourEmail.value = "";
-  YourPhone.value = "";
-}
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "faresshessen61@gmail.com",
+//     Password: "DB87645B09CFA82F10DB8793B350DBDD0679",
+//     To: "faresshessen61@gmail.com",
+//     From: "faresshessen61@gmail.com",
+//     Subject: YourName.value,
+//     Body: bodyMassege,
+//   });
+//   YourName.value = "";
+//   YourEmail.value = "";
+//   YourPhone.value = "";
+// }
 
-Send.onclick = function () {
-  if (
-    YourName.value !== "" &&
-    YourEmail.value !== "" &&
-    YourPhone.value !== ""
-  ) {
-    sendEmail();
-    showPopup();
-  } else {
-    msgForm.style.display = "block";
-    msgForm.innerHTML = "Writ all Input blank";
-    setTimeout(() => {
-      msgForm.style.display = "none";
-    }, 3000);
-  }
-};
+// Send.onclick = function () {
+//   if (
+//     YourName.value !== "" &&
+//     YourEmail.value !== "" &&
+//     YourPhone.value !== ""
+//   ) {
+//     sendEmail();
+//     showPopup();
+//   } else {
+//     msgForm.style.display = "block";
+//     msgForm.innerHTML = "Writ all Input blank";
+//     setTimeout(() => {
+//       msgForm.style.display = "none";
+//     }, 3000);
+//   }
+// };
 
 // handel Popup
 let btnPopup = document.querySelector("#btnPopup");
 let overlay = document.createElement("div");
 
 //function  showPopup
-function showPopup() {
-  overlay.classList.add("overlay");
-  document.body.appendChild(overlay);
-  document.querySelector(".popup").style.display = "block";
-}
+// function showPopup() {
+//   overlay.classList.add("overlay");
+//   document.body.appendChild(overlay);
+//   document.querySelector(".popup").style.display = "block";
+// }
 
 //function  hidePopup
-btnPopup.onclick = function () {
-  overlay.remove();
-  document.querySelector(".popup").style.display = "none";
-};
+// btnPopup.onclick = function () {
+//   overlay.remove();
+//   document.querySelector(".popup").style.display = "none";
+// };
